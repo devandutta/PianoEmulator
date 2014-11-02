84a1c74dacc45502a2397ad6502cad997fe1ddd1
Devan Dutta <Devan@Devans-MacBook-Pro-2.local>
2014-11-02 00:12:58 -0700

    Initial Commit

---
+++ piano_project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  piano_project
+//
+//  Created by Devan Dutta on 11/2/14.
+//  Copyright (c) 2014 devan.dutta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
